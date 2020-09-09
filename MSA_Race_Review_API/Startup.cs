feab2c99fd6def529176103c3f1ce9beb154a547
GeorgeCLu <52149356+GeorgeCLu@users.noreply.github.com>
2020-09-09 14:37:30 +1200

    update

--- MSA_Race_Review_API/Startup.cs
+++ MSA_Race_Review_API/Startup.cs
@@ -44,8 +44,7 @@ namespace MSA_Race_Review_API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "reviewSIMS", Version = "v1" });
             });
-            // var connection = Configuration.GetConnectionString("reviewSIMSConnection");
-			var connection = Environment.GetEnvironmentVariable("reviewSIMSConnection");
+            var connection = Configuration.GetConnectionString("reviewSIMSConnection");
             services.AddDbContext<ReviewContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
             
